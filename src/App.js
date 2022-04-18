b48abf7a90ed58f59887775060680edbda5e17f0
khushaldobariya <khushaldobariya.77@gmail.com>
2022-04-17 19:26:36 -0700

    multihospital

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./componet/Header";
+import Home from "./container/Home";
+import Footer from "./componet/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
