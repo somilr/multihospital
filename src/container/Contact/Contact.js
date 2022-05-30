95e57e1656b321ebf126c1b6801cf900696f0c7b
khushaldobariya <khushaldobariya.77@gmail.com>
2022-05-30 18:46:19 +0530

    30-5-2022

--- src/container/Contact/Contact.js
+++ src/container/Contact/Contact.js
@@ -1,6 +1,38 @@
 import React from 'react';
-
+import * as yup from 'yup';
+import { Form, Formik, useFormik } from 'formik';
 function Contact(props) {
+
+    const handleSignup = (values) => {
+        let data = JSON.parse(localStorage.getItem("users"))
+
+        if (data === null) {
+            localStorage.setItem("users", JSON.stringify([values]))
+        } else {
+            data.push(values)
+            localStorage.setItem("users", JSON.stringify(data))
+        }
+    }
+
+    let schema = yup.object().shape({
+        name: yup.string().required('please enter name'),
+        email: yup.string().required('please enter email').email('enter valid email'),
+        subject: yup.string().required('please enter subject'),
+        message:yup.string().required('please enter message')
+      });
+      const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            subject: '',
+            message: ''
+        },
+        validationSchema:schema,
+        onSubmit: values => {
+            handleSignup(values)
+          },
+        });
+        console.log(formik.errors.name); 
     return (
         <section id="contact" className="contact">
             <div className="container">
@@ -33,28 +65,69 @@ function Contact(props) {
                         </div>
                     </div>
                     <div className="col-lg-8 mt-5 mt-lg-0">
-                        <form action method="post" role="form" className="php-email-form">
+                         <Formik value={formik}>
+                         <Form onSubmit={formik.handleSubmit} className="php-email-form">
                             <div className="row">
                                 <div className="col-md-6 form-group">
-                                    <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
+                                    <input
+                                     type="text" 
+                                    name="name" 
+                                    className="form-control" 
+                                    id="name" 
+                                    placeholder="Your Name" 
+                                    onChange={formik.handleChange} />
+                      {
+                          formik.errors.name && formik.touched.name ? <p>{formik.errors.name}</p> : ''
+                    }                
                                 </div>
                                 <div className="col-md-6 form-group mt-3 mt-md-0">
-                                    <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
+                                    <input
+                                     type="email" 
+                                    className="form-control"
+                                     name="email" 
+                                    id="email"
+                                     placeholder="Your Email" 
+                                     onChange={formik.handleChange} />
+                    {
+                          formik.errors.email && formik.touched.email ? <p>{formik.errors.email}</p> : ''
+                    }
                                 </div>
                             </div>
                             <div className="form-group mt-3">
-                                <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" required />
+                                <input 
+                                type="text" 
+                                className="form-control"
+                                 name="subject" 
+                                id="subject"
+                                 placeholder="Subject" 
+                                 onChange={formik.handleChange} />
+                    {
+                          formik.errors.subject && formik.touched.subject ? <p>{formik.errors.subject}</p> : ''
+                    }             
                             </div>
                             <div className="form-group mt-3">
-                                <textarea className="form-control" name="message" rows={5} placeholder="Message" required defaultValue={""} />
+                                <textarea 
+                                className="form-control"
+                                 name="message" 
+                                rows={5} 
+                                placeholder="Message"
+                                onChange={formik.handleChange}/>
+                    {
+                          formik.errors.message && formik.touched.message ? <p>{formik.errors.message}</p> : ''
+                    }            
                             </div>
                             <div className="my-3">
                                 <div className="loading">Loading</div>
                                 <div className="error-message" />
-                                <div className="sent-message">Your message has been sent. Thank you!</div>
+                                <a onAuxClick={()=>{
+                                    <div className="sent-message">Your message has been sent. Thank you!</div>
+
+                                }}></a> 
+
                             </div>
                             <div className="text-center"><button type="submit">Send Message</button></div>
-                        </form>
+                        </Form>
+                         </Formik>
                     </div>
                 </div>
             </div>
